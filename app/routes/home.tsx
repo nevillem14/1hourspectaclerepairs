e5379f255f6f3efe72f8a793cbc2b5ae93f46ab2
nevillem14 <111359293+nevillem14@users.noreply.github.com>
2025-08-24 12:27:47 +0200

    create hero

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,19 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Hero } from "~/components/Hero";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "WSDXI" },
+    { name: "description", content: "Waterfall Software Development." },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <section>
+      <div>
+        <Hero mx-0 my-0 p-0 />
+      </div>
+    </section>
+  );
 }
