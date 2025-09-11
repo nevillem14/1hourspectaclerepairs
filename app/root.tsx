26d40d776fbe8ee9c9b550a7eec8c9e6b68926f0
nevillem14 <111359293+nevillem14@users.noreply.github.com>
2025-09-11 17:06:41 +0200

    make footer sticky at bottom

--- app/root.tsx
+++ app/root.tsx
@@ -34,10 +34,16 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="flex flex-col min-h-screen">
+        {/* Navbar pinned at top */}
         <Navbar />
-        {children}
+
+        {/* Page content grows to push footer down */}
+        <main className="flex-grow pt-20">{children}</main>
+
+        {/* Footer sticks to bottom */}
         <Footer />
+
         <ScrollRestoration />
         <Scripts />
       </body>
