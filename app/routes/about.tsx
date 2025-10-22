e45aa4a819bb31da7dfcc5d3ba24eb1667d80133
nevillem14 <111359293+nevillem14@users.noreply.github.com>
2025-10-22 16:20:39 +0200

    remove rotating block from  about

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+import RotatingBlock from "~/components/RotatingBlock";
 
 export default function About() {
   return (
-    <div className="flex items-center justify-center h-screen">about Us</div>
+    <div className="flex items-center justify-center h-screen">
+      <h1>About Us</h1>
+      {/*<RotatingBlock />*/}
+    </div>
   );
 }
