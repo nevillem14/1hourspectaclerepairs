9597dbe92b88658cb1dd79a88adefed231f57bcd
nevillem14 <111359293+nevillem14@users.noreply.github.com>
2026-04-24 13:34:16 +0200

    update supabase to to target local storage explicity

--- app/utils/supabaseClient.ts
+++ app/utils/supabaseClient.ts
@@ -3,4 +3,12 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY!
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: true, // Forces session to stay after refresh
+    autoRefreshToken: true,
+    detectSessionInUrl: true,
+    // Explicitly target localStorage for browser environments
+    storage: typeof window !== "undefined" ? window.localStorage : undefined,
+  },
+});
